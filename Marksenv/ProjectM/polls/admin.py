a931724e764e3dbb16ebcb25bf6e2c6377d6486d
Markwaardig <markklaarenbeek@gmail.com>
2014-10-15 22:38:42 +0200

    customize the admin change list

--- Marksenv/ProjectM/polls/admin.py
+++ Marksenv/ProjectM/polls/admin.py
@@ -12,6 +12,7 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
     
+    
 admin.site.register(Question, QuestionAdmin)
 
 admin.site.register(Choice)
--- Marksenv/ProjectM/polls/models.py
+++ Marksenv/ProjectM/polls/models.py
@@ -5,12 +5,15 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+#	def __str__(self):              
+#		return self.Question_text
+
 #	def was_published_recently(self):
 #		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-#	def __str__(self):              
-#		return self.question_text
-
+#	was_published_recently.admin_order_field = 'pub_date'
+#    was_published_recently.boolean = True
+#    was_published_recently.short_description = 'Published recently?'		
+	
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
@@ -18,6 +21,6 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
 #	def __str__(self):             
-#		return self.choice_text
+#		return self.Choice_text
 
 
