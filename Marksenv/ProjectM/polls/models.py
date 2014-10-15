e40b6dcefc58c79f50418047360192a7d8ad0514
Markwaardig <markklaarenbeek@gmail.com>
2014-10-15 20:00:15 +0200

    added the admin functionality - sstill need to change question object to question eg 'Whats u?'

--- Marksenv/ProjectM/polls/models.py
+++ Marksenv/ProjectM/polls/models.py
@@ -5,11 +5,11 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+#	def was_published_recently(self):
+#		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-	def __str__(self):              
-		return self.question_text
+#	def __str__(self):              
+#		return self.question_text
 
 
 class Choice(models.Model):
@@ -17,7 +17,7 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-	def __str__(self):             
-		return self.choice_text
+#	def __str__(self):             
+#		return self.choice_text
 
 
