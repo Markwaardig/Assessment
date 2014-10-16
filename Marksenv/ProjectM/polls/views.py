663487ac7ba6b2ef5ee5539ae626365682bda427
Markwaardig <markklaarenbeek@gmail.com>
2014-10-16 20:56:31 +0200

    removed harcoded URLs

--- Marksenv/ProjectM/polls/views.py
+++ Marksenv/ProjectM/polls/views.py
@@ -1,20 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from polls.models import Question
 
-
+# add discription ...  
 def index(request):
     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
-
+# add discription ...  
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question': question})
 
+#    return HttpResponse("You're looking at question %s." % question_id)
+
+
+
+# add discription ...  
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
-
+# add discription ...  
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
